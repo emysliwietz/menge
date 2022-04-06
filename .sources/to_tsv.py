61fcc174d03a855454c88f0e376275dd63c8cf6a
egidius <egidius@mysliwietz.de>
2022-04-07 01:23:52 +0200

    Script now handles directories

--- .sources/to_tsv.py
+++ .sources/to_tsv.py
@@ -3,72 +3,74 @@
 import sys
 import os
 
-books = """Genesis (Ge)
-Exodus (Exo)
-Leviticus (Lev)
-Numbers (Num)
-Deuteronomy (Deu)
-Joshua (Josh)
-Judges (Jdgs)
-Ruth (Ruth)
-1 Samuel (1Sm)
-2 Samuel (2Sm)
-1 Kings (1Ki)
-2 Kings (2Ki)
-1 Chronicles (1Chr)
-2 Chronicles (2Chr)
-Ezra (Ezra)
-Nehemiah (Neh)
-Esther (Est)
-Job (Job)
-Psalms (Psa)
-Proverbs (Prv)
-Ecclesiastes (Eccl)
-Song of Solomon (SSol)
-Isaiah (Isa)
-Jeremiah (Jer)
-Lamentations (Lam)
-Ezekiel (Eze)
-Daniel (Dan)
-Hosea (Hos)
-Joel (Joel)
-Amos (Amos)
-Obadiah (Obad)
-Jonah (Jonah)
-Micah (Mic)
-Nahum (Nahum)
-Habakkuk (Hab)
-Zephaniah (Zep)
-Haggai (Hag)
-Zechariah (Zec)
-Malachi (Mal)
-Matthew (Mat)
-Mark (Mark)
-Luke (Luke)
-John (John)
-The Acts (Acts)
-Romans (Rom)
-1 Corinthians (1Cor)
-2 Corinthians (2Cor)
-Galatians (Gal)
-Ephesians (Eph)
-Philippians (Phi)
-Colossians (Col)
-1 Thessalonians (1Th)
-2 Thessalonians (2Th)
-1 Timothy (1Tim)
-2 Timothy (2Tim)
-Titus (Titus)
-Philemon (Phmn)
-Hebrews (Heb)
-James (Jas)
-1 Peter (1Pet)
-2 Peter (2Pet)
-1 John (1Jn)
-2 John (2Jn)
-3 John (3Jn)
-Jude (Jude)
-Revelation (Rev)"""
+books = {
+    "Genesis": ["Ge", 1],
+    "Exodus": ["Exo", 2],
+    "Levitikus": ["Lev", 3],
+    "Numeri": ["Num", 4],
+    "Deuteronomium": ["Deu", 5],
+    "Josua": ["Josh", 6],
+    "Richter": ["Jdgs", 7],
+    "Rut": ["Ruth", 8],
+    "1. Samuel": ["1Sm", 9],
+    "2. Samuel": ["2Sm", 10],
+    "1. Könige": ["1Ki", 11],
+    "2. Könige": ["2Ki", 12],
+    "1. Chronik": ["1Chr", 13],
+    "2. Chronik": ["2Chr", 14],
+    "Esra": ["Ezra", 15],
+    "Nehemia": ["Neh", 16],
+    "Ester": ["Est", 17],
+    "Hiob": ["Job", 18],
+    "Psalmen": ["Psa", 19],
+    "Sprüche": ["Prv", 20],
+    "Prediger": ["Eccl", 21],
+    "Hoheslied": ["SSol", 22],
+    "Jesaja": ["Isa", 23],
+    "Jeremia": ["Jer", 24],
+    "Klagelieder": ["Lam", 25],
+    "Hesekiel": ["Eze", 26],
+    "Daniel": ["Dan", 27],
+    "Hosea": ["Hos", 28],
+    "Joel": ["Joel", 29],
+    "Amos": ["Amos", 30],
+    "Obadja": ["Obad", 31],
+    "Jona": ["Jonah", 32],
+    "Micha": ["Mic", 33],
+    "Nahum": ["Nahum", 34],
+    "Habakuk": ["Hab", 35],
+    "Zefanja": ["Zep", 36],
+    "Haggai": ["Hag", 37],
+    "Sacharja": ["Zec", 38],
+    "Maleachi": ["Mal", 39],
+    "Matthew": ["Mat", 40],
+    "Mark": ["Mark", 41],
+    "Luke": ["Luke", 42],
+    "John": ["John", 43],
+    "The Acts": ["Acts", 44],
+    "Romans": ["Rom", 45],
+    "1 Corinthians": ["1Cor", 46],
+    "2 Corinthians": ["2Cor", 47],
+    "Galatians": ["Gal", 48],
+    "Ephesians": ["Eph", 49],
+    "Philippians": ["Phi", 50],
+    "Colossians": ["Col", 51],
+    "1 Thessalonians": ["1Th", 52],
+    "2 Thessalonians": ["2Th", 53],
+    "1 Timothy": ["1Tim", 54],
+    "2 Timothy": ["2Tim", 55],
+    "Titus": ["Titus", 56],
+    "Philemon": ["Phmn", 57],
+    "Hebrews": ["Heb", 58],
+    "James": ["Jas", 59],
+    "1 Peter": ["1Pet", 60],
+    "2 Peter": ["2Pet", 61],
+    "1 John": ["1Jn", 62],
+    "2 John": ["2Jn", 63],
+    "3 John": ["3Jn", 64],
+    "Jude": ["Jude", 65],
+    "Revelation": ["Rev", 66],
+}
 
 apocrypha = {
     "Tobit": ["Tob", 67],
@@ -81,48 +83,65 @@ apocrypha = {
     "2. Makkabäer": ["2Mac", 77],
     "Zusätze Ester": ["AddEst", 78],
     "Gebet Manasses": ["Man", 79],
-    "Zusätze Daniel": ["AddDan", 81]
+    "Zusätze Daniel": ["AddDan", 81],
 }
 
-book_abbrevs = []
-for book in books.split("\n"):
-    book_abbrevs.append(book.split("(")[1].split(")")[0].strip())
+
+def parse_book(to_read):
+    file_name = os.path.basename(to_read)
+    book_name = file_name.split(" - ")[1].strip().split(".txt")[0].strip()
+
+    if book_name in books:
+        book_abbrev = books[book_name][0]
+        book_number = books[book_name][1]
+    elif book_name in apocrypha:
+        book_abbrev = apocrypha[book_name][0]
+        book_number = apocrypha[book_name][1]
+    else:
+        print(f"Unable to find name of: {file_name}")
+        return
+
+    print(f"{book_number} {book_name} {book_abbrev}")
+
+    curr_chapter = -1
+    new_lines = [""]
+    is_text = False
+
+    with open(file_name, "r") as f:
+        old_lines = f.readlines()
+
+        for line in old_lines:
+            if line.startswith("Kapitel "):
+                curr_chapter = line.split("Kapitel ")[1].strip()
+            elif curr_chapter != -1 and line.split(" ")[0].isdigit():
+                l = line.split(" ")
+                verse_num = l[0]
+                verse = " ".join(l[1:])
+                new_lines.append(
+                    f"{book_name}\t{book_abbrev}\t{book_number}\t{curr_chapter}\t{verse_num}\t{verse}".replace(
+                        "\n", "\r\n"
+                    )
+                )
+                is_text = True
+            elif line.isspace():
+                is_text = False
+            elif curr_chapter != -1 and is_text:
+                last_line = new_lines.pop().replace("\r\n", " ")
+                last_line += line.replace("\n", "\r\n")
+                new_lines.append(last_line)
+                print(last_line)
+            else:
+                pass
+
+    with open("books/" + book_name + ".tsv", "w") as f:
+        f.writelines(new_lines)
 
 
-to_read = sys.argv[1]
-file_name = os.path.basename(to_read)
-book_number = file_name.split(" - ")[0].strip()
+if len(sys.argv) == 1:
+    print(f"Usage: {sys.argv[0]} {{source_dir}}")
+    exit()
 
-if len(sys.argv) == 3:
-    book_number = sys.argv[2]
-
-book_name = file_name.split(" - ")[1].strip().split(".txt")[0].strip()
-
-book_abbrev = book_abbrevs[int(book_number) - 1]
-
-print(book_name)
-if book_name in apocrypha:
-    book_abbrev = apocrypha[book_name][0]
-    book_number = apocrypha[book_name][1]
-
-print(book_abbrev)
-print(book_number)
-
-curr_chapter = -1
-new_lines = [""]
-
-
-with open(to_read, "r") as f:
-    old_lines = f.readlines()
-
-for line in old_lines:
-    if line.startswith("Kapitel "):
-        curr_chapter = line.split("Kapitel ")[1].strip()
-    elif curr_chapter != -1 and line.split(" ")[0].isdigit():
-        l = line.split(" ")
-        verse_num = l[0]
-        verse = " ".join(l[1:])
-        new_lines.append(f"{book_name}\t{book_abbrev}\t{book_number}\t{curr_chapter}\t{verse_num}\t{verse}".replace("\n", "\r\n"))
-
-with open("books/" + book_name + ".tsv", "w") as f:
-    f.writelines(new_lines)
+for f in os.listdir(sys.argv[1]):
+    f = os.path.join(sys.argv[1], f)
+    if os.path.isfile(f):
+        parse_book(f)
